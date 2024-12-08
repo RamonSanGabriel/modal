bc6fe7b64172f7db94e80d617b2dc05a5d9d8f2e
RamonSanGabriel <jeshero11@gmail.com>
2024-12-08 13:31:56 +0800

    message value

--- src/App.jsx
+++ src/App.jsx
@@ -4,15 +4,31 @@ import Modal from './components/Modal/Modal';
 
 function App() {
   const [showModal, setShowModal] = useState(false);
+  const [message, setMessage] = useState('');
 
   const handleShowModal = () => {
     setShowModal(true);
   };
+  const handleCloseModal = (value) => {
+    setShowModal(false);
+    setMessage(value);
+  };
 
   return (
     <div>
+      {message === '' ? (
+        <p>No button was clicked</p>
+      ) : (
+        <p>
+          You clicked the <strong>{message}</strong> button{' '}
+        </p>
+      )}
       {showModal ? (
-        <Modal onClose={setShowModal} onSubmit={null} onCancel={null} />
+        <Modal
+          onClose={handleCloseModal}
+          onSubmit={handleCloseModal}
+          onCancel={handleCloseModal}
+        />
       ) : (
         <button className="show-modal-btn" onClick={handleShowModal}>
           Show Modal
--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,19 +1,19 @@
 import css from './Modal.module.css';
 
-const Modal = ({ onClose, onSubmit, onCancel, setShowModal }) => {
+const Modal = ({ onClose, onSubmit, onCancel, handleCloseModal }) => {
   return (
     <div className={css.modal}>
       <div className={css.modalContent}>
         <div className={css.close}>
-          <p onClick={() => onClose(setShowModal)}>&times;</p>{' '}
+          <p onClick={() => onClose('close')}>&times;</p>{' '}
         </div>
         <h1>This is a modal</h1>
         <p>It shows the functionality of a modal</p>
         <div className={css.footerBtns}>
-          <button onClick={() => onSubmit} className={css.btnSubmit}>
+          <button onClick={() => onSubmit('submit')} className={css.btnSubmit}>
             Submit
           </button>
-          <button onClick={() => onCancel} className={css.btnCancel}>
+          <button onClick={() => onCancel('cancel')} className={css.btnCancel}>
             Cancel
           </button>
         </div>
